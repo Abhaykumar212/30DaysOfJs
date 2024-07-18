967dcd3d0ed3800f778437739c484b14ea93f1cd
Abhay kumar <mk9416822@gmail.com>
2024-07-19 00:43:25 +0530

    day1 task Done✅

---
+++ day1.js
@@ -0,0 +1,29 @@
+var a = 10;
+console.log(a);
+
+let b = "Hello World!";
+console.log(b);
+
+const c = true;
+console.log(c);
+
+let d = 42;
+let e = "JavaScript";
+let f = false;
+let g = { name: "Abhay", age: 18};
+let h = [1, 2, 3, 4, 5];
+
+console.log(d, typeof d);
+console.log(e, typeof e);
+console.log(f, typeof f);
+console.log(g, typeof g);
+console.log(h, typeof h);
+
+let i = 100;
+console.log(i);
+i = 200;
+console.log(i);
+
+const j = 5;
+console.log(j);
+
