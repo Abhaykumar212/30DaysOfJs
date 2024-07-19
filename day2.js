2945fb304a2010cb30822bfee72d377a01af2b77
Abhay kumar <mk9416822@gmail.com>
2024-07-20 01:11:13 +0530

    day2 tasks done successfully

---
+++ day2.js
@@ -0,0 +1,23 @@
+let a = 5, b = 3;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+console.log(a-=2, b+=2);
+
+console.log(a>b);
+console.log(a<b);
+console.log(a==b);
+let c = 3, d = 3;
+console.log(c===d);
+
+console.log(a>b && a<b);
+console.log(a<b || a<b);
+console.log(!(a==b));
+
+let e = -6;
+
+console.log(e<0 ? true : false);
+
